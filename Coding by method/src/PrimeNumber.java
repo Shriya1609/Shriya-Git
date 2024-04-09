850eb734ba229e782e10ce6ecb5c17a2c0d271cf
Shriya1609 <sharmashriya417@gmail.com>
2024-04-08 21:26:40 -0300

    adding prime number code by method

--- Coding by method/src/PrimeNumber.java
+++ Coding by method/src/PrimeNumber.java
@@ -1,2 +1,23 @@
-package PACKAGE_NAME;public class PrimeNumber {
+import java.util.Scanner;
+
+public class PrimeNumber {
+
+    public static void main(String[] args) {
+        PrimeNumber pn=new PrimeNumber();
+        pn.Primenumber(25);
+
+    }
+
+    public int Primenumber(int n){
+
+            if(n%2==0){
+                System.out.println("Number is  even:"+n);
+            }else
+                System.out.println("Number is odd:"+n);
+
+
+        return n;
+    }
+
+
 }
